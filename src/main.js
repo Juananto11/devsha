67d5e099ae0106933734aa179516e91507d3c68f
Juananto11 <juananto11@hotmail.com>
2018-03-26 22:22:40 -0600

    Cambio de Element-Ui a Vue Material

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
 import router from './router/'
 import store from './store/'
 
-Vue.use(Element)
+Vue.use(VueMaterial)
 Vue.config.productionTip = false
 
 new Vue({
